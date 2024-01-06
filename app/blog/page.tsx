7aafec345a524ab2977f437aadf8e33b418240dc
me505 <jainarjav886@gmail.com>
2024-01-06 12:32:03 +0530

    feat: minor improvements

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { simpleBlogCard } from "./lib/interface";
-import { client, urlFor } from "./lib/sanity";
+import { simpleBlogCard } from "../lib/interface";
+import { client, urlFor } from "../lib/sanity";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
