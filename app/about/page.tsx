7ecf0505e6a11bf758cfabf25c66398be68660c5
me505 <jainarjav886@gmail.com>
2024-01-07 15:37:38 +0530

    make the about me better

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,19 +17,19 @@ export default function About() {
             About me
           </span>
         </div>
-        <div className="mt-10 flex-col max-w-2xl items-center justify-center mx-auto font-light">
-          <h1 className="text-xl text-justify">
+        <div className="mt-10 flex-col max-w-2xl mr-5 items-center justify-center mx-auto font-light">
+          <h1 className="text-xl ">
             Hi, My name is Aarjav Jain. I am currently studying in 9th.
             I am mostly interested in making weird projects from weird apis, libraries and frameworks in the
             <span className="hover:underline text-primary hover:cursor-pointer"> typescript </span>
             language while also simultaneously trying to learn systems programming which has been a hard rock to break.
           </h1>
-          <h1 className="text-xl text-justify mt-7">
+          <h1 className="text-xl  mt-7">
             My current interests are to program while not being a total shut-in and maintain my social life.
             For the future i havent reall thought about what i am gonna do. But knowing myself, I know its gonna be something funny
           </h1>
 
-          <h1 className="text-xl text-justify mt-7">
+          <h1 className="text-xl mt-7">
             As for now, My current projects which i have done arent noteworthy enough to showcase, I will add them when i make them
           </h1>
         </div>
